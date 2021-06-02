82c315c0e85501903f16239bbed27b44124db136
Emmanuel Spiro <45499938+purpl3dragon@users.noreply.github.com>
2021-06-02 14:27:30 -0700

    Update Multiples of 3 or 5.py

--- Multiples of 3 or 5.py
+++ Multiples of 3 or 5.py
@@ -9,20 +9,24 @@ def solution(number):
     #Check if number entered is negative
     if (number < 0):
         return 0
+    
     list = []
-    print(number)
     multiples = []
+    
     #get a countdown list of the number you had starting with number-1 not including 0
     while (number >1):
         number = number - 1
         list.append(number)
+        
     #check if number in list is multiple of 3 or 5 
     for x in list:
         if (x % 3 == 0 or x % 5 == 0):
             multiples.append(x)
+            
     a = len(multiples)
     sum = 0
-    #Add all of the multiples together
+    
+    #Add all of the multiples together and return sum
     while a > 0:
         a -= 1
         sum += multiples[a]
